aaaec2ed3caabba029926b0f29645e22db8f70e2
4uuu Nya <i@qvq.im>
2019-10-20 21:13:10 +0800

    update

--- src/components/Award.js
+++ src/components/Award.js
@@ -1,14 +1,55 @@
 import React from 'react';
+import {Segment, Container, Header, Divider, Icon, Feed} from 'semantic-ui-react';
 
 export default class AwardComponent extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            loading: true,
+            items: {}
+        }
+    }
+
+    componentDidMount() {
+        fetch('/data/award.json')
+            .then(res => res.json())
+            .then((data) => { this.setState({loading: false, items: data})})
     }
 
     render() {
-        return (
-            "Award"
-        )
+        if(this.state.loading) {
+            return (
+                "Awards Loading..."
+            )
+        } else {
+            return (
+                <Segment inverted vertical>
+                     <Container style={{ minHeight: 300, padding: "4em 0em"}}>
+                        <Header as='h2' icon inverted textAlign='center'>
+                        <Icon name='chess queen' />
+                        Awards
+                        </Header>
+                        <Divider />
+                    </Container>
+                    <Container style={{ minHeight: 500, }}>
+                        <Feed>
+                            {this.state.items.map((item, index ) =>
+                                <Feed.Event>
+                                    <Feed.Content>
+                                        <Feed.Summary style={{ color: "white" }}>
+                                            {item.title}
+                                            <Feed.Date>{item.date}</Feed.Date>
+                                        </Feed.Summary>
+                                        <Feed.Extra text style={{ color: "white" }}>
+                                            {item.desc}
+                                        </Feed.Extra>
+                                    </Feed.Content>
+                                </Feed.Event>
+                            )}
+                        </Feed>
+                    </Container>
+                </Segment>
+            )
+        }
     }
 }
