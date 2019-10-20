f3ab43bb460ada63996d64ad15e5499bc1d5af00
Ubuntu <ubuntu@ip-172-31-24-197.ap-east-1.compute.internal>
2019-10-20 06:49:58 +0000

    la

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Switch,
+  Route,
+  NavLink
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <ul>
+          <li><NavLink to="/" activeClassName="active"> Home </NavLink></li>
+          <li><NavLink to="/members" activeClassName="active"> Members </NavLink></li>
+          <li><NavLink to="/awards" activeClassName="active"> Awards </NavLink></li>
+          <li><NavLink to="/contact" activeClassName="active"> Contact Us </NavLink></li>
+        </ul>
+      </div>
+      <div>
+        <Switch>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+
+          <Route path="/members">
+            <h1>Members</h1>
+          </Route>
+
+          <Route path="/awards">
+            <h1>Awards</h1>
+          </Route>
+
+          <Route path="/contact">
+            <h1>Contact Us</h1>
+          </Route>
+
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
