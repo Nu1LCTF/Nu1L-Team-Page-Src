b352640bd792cf4d8c63329416c5cf77b33c2842
Manasseh Zhou <manassehzhou@gmail.com>
2019-10-21 14:43:17 +0000

    fix menu

--- src/components/partial/Menu.js
+++ src/components/partial/Menu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import {
+    Icon,
     Image,
     Container,
     Menu,
@@ -13,11 +14,11 @@ const SMenu = () => (
         style={{ padding: '1em 0em' }} >
         <Menu size='large' pointing secondary inverted style={{ border: "0px" }}>
             <Container>
-                <Menu.Item as={NavLink} name='home' to='/' activeClassName='active' exact> Home </Menu.Item>
+                <Menu.Item as={NavLink} name='home' to='/' activeClassName='active' exact><Icon name='home' /></Menu.Item>
                 <Menu.Item as='a' name='GitHub' href="https://github.com/Nu1LCTF/">GitHub</Menu.Item>
                 <Menu.Item as='a' name='Twitter' href="https://twitter.com/Nu1L_Team">Twitter</Menu.Item>
                 <Popup trigger={<Menu.Item name='WeChat'>WeChat</Menu.Item>} ><Image size='small' src='images/wechat.jpg'></Image></Popup>
-                <Menu.Item as='a' name='Write-ups' href="https://wx.zsxq.com/dweb2/index/group/824215518412">Write-ups</Menu.Item>
+                <Menu.Item as='a' name='Writeups' href="https://wx.zsxq.com/dweb2/index/group/824215518412">Writeup</Menu.Item>
             </Container>
         </Menu>
 
