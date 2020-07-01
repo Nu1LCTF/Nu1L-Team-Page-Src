f01378c8e8548ef7fff6309157d6b04ca4600567
Manasseh Zhou <2705803+ManassehZhou@users.noreply.github.com>
2020-07-01 19:25:44 +0800

    update email again

--- src/components/partial/JoinUsModal.js
+++ src/components/partial/JoinUsModal.js
@@ -41,7 +41,7 @@ export default class JoinUsModal extends Component {
                 <Header icon='user secret' content='Tell Us More About Youself' />
                 <Modal.Content>
                     <p>
-                        Send following data to our email 'root@Nu1L.com'.
+                        Send following data to our email 'root@nu1l.com'.
             </p>
                     <List>
                         <List.Item>ID: `Incognito`</List.Item>
